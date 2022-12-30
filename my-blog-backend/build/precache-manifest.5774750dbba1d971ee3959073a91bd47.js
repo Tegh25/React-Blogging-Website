5324c64ed22ff68c7118899b7484598bc3c7282f
Tegh25 <teghveera@gmail.com>
2022-12-30 11:39:02 -0600

    added build

---
+++ my-blog-backend/build/precache-manifest.5774750dbba1d971ee3959073a91bd47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f65b66d53667dec77e3f",
+    "url": "/static/css/main.d9c1998b.chunk.css"
+  },
+  {
+    "revision": "f65b66d53667dec77e3f",
+    "url": "/static/js/main.f65b66d5.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "d28aa0137570a8b42e4f",
+    "url": "/static/js/2.d28aa013.chunk.js"
+  },
+  {
+    "revision": "fb573bd4075a37a7f80b",
+    "url": "/static/js/runtime~main.fb573bd4.js"
+  },
+  {
+    "revision": "282df1a5bd20380247eba5c5b55db93f",
+    "url": "/index.html"
+  }
+];
