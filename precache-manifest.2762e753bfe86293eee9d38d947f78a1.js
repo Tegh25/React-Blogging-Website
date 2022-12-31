21d5d136d69daaf599ff045ccc7ffa61997e3922
Tegh25 <teghveera@gmail.com>
2022-12-30 19:23:51 -0600

    Updates

---
+++ precache-manifest.2762e753bfe86293eee9d38d947f78a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f65b66d53667dec77e3f",
+    "url": "/Blogging-Website/static/css/main.d9c1998b.chunk.css"
+  },
+  {
+    "revision": "f65b66d53667dec77e3f",
+    "url": "/Blogging-Website/static/js/main.f65b66d5.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/Blogging-Website/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "520e2094418f935e35d6",
+    "url": "/Blogging-Website/static/js/2.520e2094.chunk.js"
+  },
+  {
+    "revision": "9f9f85cdd2abfac3f026",
+    "url": "/Blogging-Website/static/js/runtime~main.9f9f85cd.js"
+  },
+  {
+    "revision": "7f73372863dd0123c75bae6485499a89",
+    "url": "/Blogging-Website/index.html"
+  }
+];
